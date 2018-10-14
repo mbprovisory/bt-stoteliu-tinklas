eb46691431c74385d9d36f0570a98271ea6cbaf2
mnaujokaitis <mnaujokaitis@users.noreply.github.com>
2018-10-14 19:32:59 +0300

    BT-0000 | internationalize

---
+++ src/main/java/lt/baltictalents/stoteliutinklas/data/beans/Station.java
@@ -0,0 +1,49 @@
+package lt.baltictalents.stoteliutinklas.data.beans;
+
+public class Station {
+	String name;
+	String longtitute;
+	String latitude;
+	String[] routes;
+
+	public Station(String name, String lon, String lat, String[] routes) {
+		super();
+		this.name = name;
+		this.longtitute = lon;
+		this.latitude = lat;
+		this.routes = routes;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLongtitute() {
+		return longtitute;
+	}
+
+	public void setLongtitute(String longtitute) {
+		this.longtitute = longtitute;
+	}
+
+	public String getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+
+	public String[] getRoutes() {
+		return routes;
+	}
+
+	public void setRoutes(String[] routes) {
+		this.routes = routes;
+	}
+
+}
