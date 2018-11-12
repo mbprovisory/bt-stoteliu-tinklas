6e5e84aefb990ff64a4cdbb2443cd06f769ab3bf
mnaujokaitis <m.naujokaitis@gmail.com>
2018-11-12 20:04:19 +0200

    Reorganise folder structure and add new HelloWorld

---
+++ src/main/java/lt/baltictalents/stoteliutinklas/pavyzdys/SampleResponse.java
@@ -0,0 +1,13 @@
+package lt.baltictalents.stoteliutinklas.pavyzdys;
+
+public class SampleResponse {
+	private String winningSign; // laimintis, zenklas, pvz. sulinys ar zirkles
+
+	public String getWinningSign() {
+		return winningSign;
+	}
+
+	public void setWinningSign(String winningSign) {
+		this.winningSign = winningSign;
+	}
+}
