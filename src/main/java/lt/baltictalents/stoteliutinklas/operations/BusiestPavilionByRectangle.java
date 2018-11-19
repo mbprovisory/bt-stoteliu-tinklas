b29119ada78b7c3d1134603182ac809a78fd0ecb
Rimantas-R <riroza@gmail.com>
2018-11-19 15:06:59 +0200

    BusiestPavilionByRectangle, PavilionsByName, StationsAndPavilionsByRouteNumber klasės. Veikimas - taip

--- src/main/java/lt/baltictalents/stoteliutinklas/operations/BusiestPavilionByRectangle.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/operations/BusiestPavilionByRectangle.java
@@ -1,6 +1,8 @@
 package lt.baltictalents.stoteliutinklas.operations;
 
 import java.util.List;
+//import java.util.Optional; //paemę mažą stačiakampį gausime tuščią list'ą, todėl r. naudoti šitą;
+
 import static java.util.stream.Collectors.toList;
 
 import java.util.Comparator;
@@ -25,6 +27,28 @@ public class BusiestPavilionByRectangle {
 	return getBusiestPavilionsFromSortedList(getStationsSortedByRoutesThenNames((getInRectangle(rectangle)))); 
 	}
     
+    public List<Station> getOpt(String longit1, String latit1, String longit2, String latit2){ //integruotas metodas;
+	Coordinates corner1 = new Coordinates(longit1, latit1);
+	Coordinates corner2 = new Coordinates(longit2,latit2);
+	Rectangle rectangle = new Rectangle(corner1, corner2);
+	
+	Comparator<? super Station> comparatorByRoutesThenNames = (st1, st2)  -> {if (st1.getRoutes().length-st2.getRoutes().length==0) {
+	    return st1.getName().compareTo(st2.getName());
+	} else {return st1.getRoutes().length - st2.getRoutes().length;}
+	};
+	
+	List<Station> stotelesOI = connection.getStoteles()
+		.stream()
+		.filter(st ->  rectangle.contains(new Coordinates(st.getLongtitute(),st.getLatitude())))
+		.sorted(comparatorByRoutesThenNames )
+		.collect(toList());
+	
+	int routesCountMax = stotelesOI.get(0).getRoutes().length;
+	
+	List<Station>busiestPavilion = stotelesOI.stream().filter(st -> st.getRoutes().length==routesCountMax).collect(toList());
+	return busiestPavilion;
+	 }
+    
     public List<Station> getInRectangle(Rectangle rectangle){
 	if(rectangle ==(null)) {return connection.getStoteles();}// TODO equals metodas?
 	List<Station> stotelesInRect = 
@@ -37,7 +61,7 @@ public class BusiestPavilionByRectangle {
     }
     
     public List<Station> getStationsSortedByRoutesThenNames(List<Station> stations){
-	Comparator<? super Station> comparatorByRoutesThenNames = (st1, st2)  -> {if (st1.getRoutes().length-st2.getRoutes().length==0) {
+	Comparator<? super Station> comparatorByRoutesThenNames = (st2, st1)  -> {if (st1.getRoutes().length-st2.getRoutes().length==0) {
 	    return st1.getName().compareTo(st2.getName());
 	} else {return st1.getRoutes().length - st2.getRoutes().length;}
 	};
@@ -54,4 +78,5 @@ public class BusiestPavilionByRectangle {
 	return busiestPavilion;
     }
 
+    
 }
