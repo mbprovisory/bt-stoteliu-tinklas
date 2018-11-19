c4bde4e1a21206e7edf95efc811a49888912d881
mb.provisory <mb.provisory@gmail.com>
2018-11-19 17:36:20 +0200

    force push

--- src/main/java/lt/baltictalents/stoteliutinklas/config/Application.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/config/Application.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
+    	//force push
         //SpringApplication.run(new Class[] { Application.class }, args);
     	MainProcess.HandleArgs(args);
     }
