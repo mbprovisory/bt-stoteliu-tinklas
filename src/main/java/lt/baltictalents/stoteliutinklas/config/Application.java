59664fa395525ccb087cc0aa3d2da49d42593a69
Gennady Tarasov <gennadytarasov9@gmail.com>
2018-11-19 16:12:16 +0200

    atnaujinimas

--- src/main/java/lt/baltictalents/stoteliutinklas/config/Application.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/config/Application.java
@@ -1,5 +1,8 @@
 package lt.baltictalents.stoteliutinklas.config;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -11,7 +14,17 @@ import org.springframework.context.annotation.ComponentScan;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(new Class[] { Application.class }, args);
+        //SpringApplication.run(new Class[] { Application.class }, args);
+    	ArrayList<Integer> list = new ArrayList<Integer>();
+	    for (int i = 0; i < 6; i++){
+	        list.add(i);
+	    }
+	    int x = 0;
+	    Iterator<Integer> it = list.iterator();
+	    while (it.hasNext()){
+	        x +=it.next();
+	    }
+	    System.out.println(x);
     }
 }
 
