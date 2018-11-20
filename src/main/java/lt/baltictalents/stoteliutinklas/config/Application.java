9357f7613b6aefd122630d9b2048c895bc6c2817
paulius999 <44171462+paulius999@users.noreply.github.com>
2018-11-20 17:26:26 +0200

    Paulius comitina i locale

--- src/main/java/lt/baltictalents/stoteliutinklas/config/Application.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/config/Application.java
@@ -1,6 +1,11 @@
 package lt.baltictalents.stoteliutinklas.config;
 
+import lt.baltictalents.stoteliutinklas.data.beans.Station;
+import lt.baltictalents.stoteliutinklas.data.layer.DataListFactory;
 import lt.baltictalents.stoteliutinklas.helper.MainProcess;
+import lt.baltictalents.stoteliutinklas.operations.*;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,7 +20,10 @@ public class Application {
     public static void main(String[] args) {
     	//force push
         //SpringApplication.run(new Class[] { Application.class }, args);
-    	MainProcess.HandleArgs(args);
+    	//MainProcess.HandleArgs(args);
+    	
+    	Station listas = new NearestPavilionByCoords(new DataListFactory()).get("25.28", "54.74");
+    	System.out.println("----------------------\n" + listas + "\n----------------------" );
     }
 }
 
