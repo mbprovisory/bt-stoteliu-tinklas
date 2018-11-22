42ceb7dcba3f48c1f6903bdd8c60122290b875b2
paulius999 <paulblazys@gmail.com>
2018-11-22 15:39:52 +0200

    Paulius 7 uzduotis + testas

--- src/test/java/lt/baltictalents/stoteliutinklas/tests/TestNearestPavilionByCoords.java
+++ src/test/java/lt/baltictalents/stoteliutinklas/tests/TestNearestPavilionByCoords.java
@@ -5,20 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import lt.baltictalents.stoteliutinklas.data.layer.DataListFactory;
+import lt.baltictalents.stoteliutinklas.operations.NearestPavilionByCoords;
 
 public class TestNearestPavilionByCoords {
 
 	DataListFactory connection = new DataListFactory();
+	NearestPavilionByCoords pavilionas = new NearestPavilionByCoords(connection);
 	
 	
 	@Test
 	public void testNearestPavilionByCoords() {
-		assertTrue(true);
+		assertEquals("Kalvarijos", pavilionas.get("25.28", "54.74").get().getName()); 
+		assertEquals("Naujakiemio st.", pavilionas.get("25.2814121", "54.7238768").get().getName());
+		assertEquals("Ąžuolyno st.", pavilionas.get("25.2353227", "54.7047766").get().getName());
+		assertEquals("Ateities st.", pavilionas.get("25.2658484", "54.7374934").get().getName());
 	}
 
 	@Test
 	public void testGet() {
 		assertTrue(true);
+	}
+	
+	@Test
+	public void testGit() {
+		assertFalse(false);
 	}
 
 }
