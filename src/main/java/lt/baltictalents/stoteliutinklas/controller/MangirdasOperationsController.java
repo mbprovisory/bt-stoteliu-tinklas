8a87fcd62a8ccc72598ab2296e05c82800f1de8b
mb.provisory <mb.provisory@gmail.com>
2018-12-10 17:25:33 +0200

    Updated JSON operation: ByRadius

--- src/main/java/lt/baltictalents/stoteliutinklas/controller/MangirdasOperationsController.java
+++ src/main/java/lt/baltictalents/stoteliutinklas/controller/MangirdasOperationsController.java
@@ -1,6 +1,7 @@
 package lt.baltictalents.stoteliutinklas.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.json.JSONObject;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import lt.baltictalents.stoteliutinklas.data.beans.Station;
+import lt.baltictalents.stoteliutinklas.data.layer.DataListFactory;
+import lt.baltictalents.stoteliutinklas.operations.PavilionsByRadius;
+import lt.baltictalents.stoteliutinklas.operations.PavilionsByRadius.Measurement;
 import lt.baltictalents.stoteliutinklas.pavyzdys.SampleResponse;
 import lt.baltictalents.stoteliutinklas.pavyzdys.SampleService;
 
@@ -23,16 +28,46 @@ public class MangirdasOperationsController {
 	@Autowired
 	SampleService sampleService;
 	
-	@RequestMapping(path="/greeting", method=RequestMethod.GET, produces="application/json; charset=UTF-8")
+	/*
+	 {
+	 	"Clon":"25",
+	 	"Clat":"54",
+	 	"radius":100000,
+	 	"measurement":"meters"
+	 }
+	 
+	 */
+	@RequestMapping(path="/PavilionsByRadius", method=RequestMethod.GET, produces="application/json; charset=UTF-8")
     public Map<String, String> sampleResponse(@RequestBody String body) {
+		DataListFactory connection = new DataListFactory();
+		connection.setConnectionType(1); //HARDCODED DB
+		PavilionsByRadius pavilionsByRadius = new PavilionsByRadius(connection);
+		
 		
 		JSONObject jsonObj=new JSONObject(body);
-		String name=jsonObj.getString("name");
-		//return “success”;
+		String Clon=jsonObj.getString("Clon");
+		String Clat=jsonObj.getString("Clat");
+		Double radius=jsonObj.getDouble("radius");
+		String measurement=jsonObj.getString("measurement");
+		
+		Measurement measure;
+		if(measurement.equalsIgnoreCase("radius")) measure = Measurement.RADIUS;
+		else measure = Measurement.METERS;
+		
+		List<Station> results = pavilionsByRadius.get(Clon, Clat, radius, measure);
+		
 		HashMap<String, String> map = new HashMap<>();
-	    map.put("key", name+" modified ");
-	    map.put("foo", "bar");
-	    map.put("aa", "bb");
+		
+	    int line = 0;
+	    for(Station s : results)
+	    {	
+	    	map.put(Integer.toString(line), s.toString());
+	    	line++;
+	    }
+	    map.put("clon", Clon);
+	    map.put("clat", Clat);
+	    map.put("radius", Double.toString(radius));
+	    map.put("measurement", measurement);
 	    return map;
 		
 		
